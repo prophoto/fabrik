cafc76643191c2581f0a332d99171681a95244b8
prophoto <gregorycazillo@gmail.com>
2018-01-02 15:34:30 -0500

    fixed empty classes
    This allows use of tooltips in place of popovers for better mobile validation message handling.

--- components/com_fabrik/views/form/tmpl/bootstrap/default_group_labels_above.php
+++ components/com_fabrik/views/form/tmpl/bootstrap/default_group_labels_above.php
@@ -17,7 +17,7 @@ $element = $this->element;
 <?php echo $element->label;?>
 
 <?php if ($this->tipLocation == 'above') : ?>
-	<span class=""><?php echo $element->tipAbove ?></span>
+	<span class="tipabove"><?php echo $element->tipAbove ?></span>
 <?php endif ?>
 
 <div class="fabrikElement">
@@ -29,9 +29,9 @@ $element = $this->element;
 </div>
 
 <?php if ($this->tipLocation == 'side') : ?>
-	<span class=""><?php echo $element->tipSide ?></span>
+	<span class="tipside"><?php echo $element->tipSide ?></span>
 <?php endif ?>
 
 <?php if ($this->tipLocation == 'below') :?>
-	<span class=""><?php echo $element->tipBelow ?></span>
+	<span class="tipbelow"><?php echo $element->tipBelow ?></span>
 <?php endif ?>
